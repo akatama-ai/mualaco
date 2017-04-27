5117089127acb3a2a2fb099ce631b332123bf477
loctai <mmo.hyipcent@gmail.com>
2017-04-27 21:04:34 +0700

    ad

--- New folder/vietnamesepack/vietnamese/catalog/language/vietnamese/module/blogcomment.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_['heading_title']     = 'Latest Comments';
-
-$_['text_comment_on']   = 'on';
-?>
--- New folder/vietnamesepack/vietnamese/catalog/language/vietnamese/module/blogtags.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_['heading_title']     = 'Tags';
-
-$_['text_notags']       = 'No tags available';
-$_['text_href_title']   = 'items tagged with';
-?>
